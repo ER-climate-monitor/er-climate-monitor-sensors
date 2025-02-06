89b4bdf93d62312a37907617550ae292b787d1c2
S-furi <stefano.furi@studio.unibo.it>
2025-02-06 14:48:36 +0100

    feat: create sensor from generated template if command line argument <create> is provided

--- sensor/build_mockup_sensors.py
+++ sensor/build_mockup_sensors.py
@@ -3,6 +3,11 @@
 import yaml
 import random
 import os
+import sys
+import subprocess
+import logging
+
+logger = logging.getLogger('TempalteCreator')
 
 selected_sensors = {
     "idro_level": [
@@ -86,10 +91,8 @@ def generate_sensor_config(name: str, sensor_type: str, port: int, ip: str) -> d
         }
     }
 
-if __name__ == "__main__":
-    directory = 'sensors_config'
-    os.makedirs(directory, exist_ok=True)
-
+def create_config_files(directory: str):
+    logger.info('Creating config file for sensor samples')
     num_sensors = sum(map(lambda x: len(x), selected_sensors.values()))
     print(num_sensors)
     ips = [ f'192.168.101.{n}' for n in random.sample(range(1, 255), num_sensors) ]
@@ -103,4 +106,24 @@ if __name__ == "__main__":
             output_file = os.path.join(directory, f'sensor_{sensor_type}_{sensor_name}.yaml')
             with open(output_file, 'w') as f:
                 yaml.safe_dump(config, f, default_flow_style=False, sort_keys=False)
+            logger.info(f"Config for sensor '{sensor_name}' of type {sensor_type} has been created succesfully at: {output_file}")
             i += 1
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    directory = 'sensors_config'
+    if (not os.path.exists(directory) and not os.listdir(directory)):
+        logger.info(f'Directory: {directory} has not been found or it is empty')
+        os.makedirs(directory, exist_ok=True)
+        create_config_files(directory)
+    else:
+        logger.info(f'files in directory {directory} have been already created')
+
+    if len(sys.argv) > 1 and sys.argv[1] == 'create':
+        logger.info('Creating sensors from yaml specifications')
+        yaml_files = [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith('.yaml')]
+        for yaml_file in yaml_files:
+            logger.info(f'Procesing {yaml_file}')
+            subprocess.run(['python3', 'create_template.py', yaml_file])
+    else:
+        logger.info("No <create> option has been provided, doing nothing...")
